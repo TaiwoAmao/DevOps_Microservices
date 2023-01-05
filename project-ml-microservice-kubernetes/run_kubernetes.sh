e11218195662bde788d2e1fe7ec4b12b7d2b8055
Ubuntu <ubuntu@ip-172-31-15-138.ec2.internal>
2023-01-05 20:26:34 +0000

    project4

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,21 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="dammyt/myproject:v1.0.0"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run myproject \
+    --image=dammyt/myproject:v1.0.0 
+    --port=80 --labels app=myproject
+
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward myproject 8000:80
 
